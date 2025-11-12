fix: clip gradients increases training stability

src/main.py
@@ -162,7 +162,7 @@ if __name__ == "__main__":
             epoch_loss += loss.item()
 
             loss.backward()
-            #torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             optimizer.step()
 
         # Logging